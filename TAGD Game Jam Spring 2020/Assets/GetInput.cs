d231fbc3686701be28368daa44e1c3012c4e81c9
Erik Priest <eriktpriest@gmail.com>
2020-02-16 00:10:53 -0600

    Changed Renderer to URP. Added basica assets for testing. Small character movement script with jumping just for testing. Messed with some 2d lights in sample scene.

---
+++ TAGD Game Jam Spring 2020/Assets/GetInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GetInput : MonoBehaviour
+{
+    private Vector2 _moveDirection = Vector2.zero;
+    private float speed = 4f;
+    private bool running = false;
+    private Rigidbody2D _rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = (running) ? 6f : 4f;
+        transform.position += new Vector3(_moveDirection.x * Time.deltaTime * speed, _moveDirection.y * Time.deltaTime * speed, 0);
+    }
+
+    void OnMovement(InputValue value)
+    {
+        _moveDirection = value.Get<Vector2>();
+    }
+
+    void OnRun(InputValue value)
+    {
+        running = (value.isPressed) ? true : false;
+    }
+
+    void OnJump()
+    {
+        _rb2d.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+    }
+}
